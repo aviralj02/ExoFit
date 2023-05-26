56195aba0f385107c269a8149afe948317901f0d
aviralj02 <aviraljain1311@gmail.com>
2023-05-27 01:10:12 +0530

    update env variable

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'ae13c4da55msh87690acafad5044p115e9cjsne8d7eb0658b3',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
@@ -9,7 +9,7 @@ export const exerciseOptions = {
  export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'ae13c4da55msh87690acafad5044p115e9cjsne8d7eb0658b3',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
